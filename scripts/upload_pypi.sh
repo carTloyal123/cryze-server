d6490db8cb64d8989cc800a3147a81ebbf406d2f
carTloyal123 <carsonloyal123@me.com>
2024-02-22 12:11:34 -0600

    remove build from git, update pypi upload script

--- scripts/upload_pypi.sh
+++ scripts/upload_pypi.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+# Get the directory of the script
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+project_dir=$script_dir/..
+echo "Script directory: $script_dir"
+echo "Project directory: $project_dir"
 
 # Remove old builds if any
 rm -rf dist build cryze_server.egg-info
 
 # increment version
-bumpversion patch
+toml_file=$project_dir/pyproject.toml
+echo "TOML file: $toml_file"
+current_version=$(cat $toml_file | grep "version" | sed -r s,"^.*=",,)
+echo "Current version: $current_version"
+
+# bump version
+bump2version --allow-dirty --current-version $current_version patch $toml_file
 
 # Build the package
-python3 setup.py sdist bdist_wheel
+python setup.py sdist bdist_wheel
 
 # Upload to PyPi
 twine upload dist/*
